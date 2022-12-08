483f736fa8a98bf8509c51a3dc97e1b7640481fb
jliebanadelgado <104687101+jliebanadelgado@users.noreply.github.com>
2022-12-08 11:40:04 +0100

    //Ejercicios_For

---
+++ Ejercicios_For/___Ejercicio_For_1_De_100_a_1.java
@@ -0,0 +1,16 @@
+public class ___Ejercicio_For_1_De_100_a_1 {
+    public static void main(String[] args) {
+
+
+	/*Exercici 1: Primers cent naturals amb poques línies de codi
+	Desenvolupa un programa que escrigui els primers 100 nombres naturals en ordre decreixent
+	(un número per línia)*/
+
+
+        int numero = 100;
+
+        for(int i = numero; i > 0; i -- ) {
+            System.out.println(numero --);
+        }
+    }
+}
