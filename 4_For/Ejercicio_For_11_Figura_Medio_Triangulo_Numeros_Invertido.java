75f72f426460a339a18891bce40a3aa7fef051d8
jliebanadelgado <104687101+jliebanadelgado@users.noreply.github.com>
2022-12-15 18:29:32 +0100

    //Exercises_Java

---
+++ 4_For/Ejercicio_For_11_Figura_Medio_Triangulo_Numeros_Invertido.java
@@ -0,0 +1,32 @@
+package For;
+
+import java.util.Scanner;
+public class Ejercicio_For_11_Figura_Medio_Triangulo_Numeros_Invertido {
+    public static void main(String[] args) {
+
+	/*Exercici 13. Un triangle invertit
+	Desenvolupa un programa que demani a l’usuari un nombre positiu més gran que 0. El
+	programa escriurà una línia per nombre entre el nombre introduït i el 1 (és a dir,
+	decreixentment). A cada línia escriurà tots els nombres des de l’1 fins el nombre corresponent
+	a la línia. Ex. si l’entrada és 5 la sortida serà
+
+	1, 2, 3, 4, 5
+	1, 2, 3, 4
+	1, 2, 3
+	1, 2
+	1*/
+
+        Scanner entrada = new Scanner(System.in);
+        int userNumber = 0;
+
+        System.out.println("Introduce un numero, por favor");
+        userNumber = entrada.nextInt();
+
+        for(int i = userNumber; i >= 1; i --) {
+            System.out.println("");
+            for(int j = 1; j <= i; j ++) {
+                System.out.print(j + " ");
+            }
+        }
+    }
+}
---
+++ 4_For/Ejercicio_For_9_Figura_Medio_Triangulo_Derechas.java
@@ -0,0 +1,31 @@
+package For;
+
+import java.util.Scanner;
+public class Ejercicio_For_9_Figura_Medio_Triangulo_Derechas {
+    public static void main(String[] args) {
+
+	/*Exercici 11. Un triangle de nombres
+	Desenvolupa un programa que demani a l’usuari un nombre positiu més gran que 0. El
+	programa escriurà una línia per nombre entre el 1 i el nombre introduït (es a dir, creixentment).
+	A cada línia escriurà un asterisc. Ex. si l’entrada és 5 la sortida serà
+
+	*
+	**
+	***
+	****
+	*****        */
+
+        Scanner entrada = new Scanner(System.in);
+        int userNumber = 0;
+
+        System.out.println("Introduce un numero, por favor");
+        userNumber = entrada.nextInt();
+
+        for(int i = 1; i <= userNumber; i++ ) {
+            System.out.println("");
+            for(int j = 1; j <= i; j++ ) {
+                System.out.print("*");
+            }
+        }
+    }
+}
